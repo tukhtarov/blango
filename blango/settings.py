d3510a430d5699726c4ba69585807b9f0edd5927
Vlad Tukhtarov <tukhtarov@gmail.com>
2023-03-30 07:57:07 +0000

    Finish logging

--- blango/settings.py
+++ blango/settings.py
@@ -142,8 +142,23 @@ class Dev(Configuration):
   CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
   CRISPY_TEMPLATE_PACK = "bootstrap5"
+  
+  LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler", "stream": "ext://sys.stdout"},
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    }
+  }
+
+  }
 
 class Prod(Dev):
     DEBUG = values.BooleanValue(True)
     SECRET_KEY = values.SecretValue()
     ALLOWED_HOSTS = values.ListValue(["localhost", "0.0.0.0", ".codio.io"])
+    
